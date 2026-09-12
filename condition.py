a07e73e7c3553013d5173e8026b088c7166746ba
SaurabhRajprogrammer <raj9279907263@gmail.com>
2026-09-12 18:14:58 +0530

    Updated File

---
+++ condition.py
@@ -0,0 +1,10 @@
+num1=int(input("enter the number1: "))
+num2=int(input("enter the number2: "))
+num3=int(input("enter the number3: "))
+
+if num1>num2 and num1>num3:
+    print("First number is largest number")
+elif num2>num1 and num2>num3:
+    print("Second number is largest number")
+else :
+    print("Third number is largest number")        
